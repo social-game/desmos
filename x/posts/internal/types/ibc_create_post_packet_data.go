36981efe57b9c0257eb839e666921d9c1e477510
riccardo.montagnin <riccardo.montagnin@gmail.com>
2020-04-07 13:04:25 +0200

    Removed custom thing from packet data

--- x/posts/internal/types/ibc_create_post_packet_data.go
+++ x/posts/internal/types/ibc_create_post_packet_data.go
@@ -3,78 +3,20 @@ package types
 import (
 	"encoding/json"
 	"fmt"
-	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/desmos-labs/desmos/x/commons"
-	"github.com/tendermint/tendermint/libs/bech32"
 )
-
-// DesmosAddress is a wrapper around sdk.AccAddress to make sure that it is properly serialized
-// using the commons.Bech32MainPrefix prefix while getting signature bytes
-type DesmosAddress struct {
-	sdk.AccAddress
-}
-
-// MarshalJSON marshals to JSON using Bech32.
-func (aa DesmosAddress) MarshalJSON() ([]byte, error) {
-	return json.Marshal(aa.String())
-}
-
-// UnmarshalJSON unmarshals from JSON assuming Bech32 encoding.
-func (aa *DesmosAddress) UnmarshalJSON(data []byte) error {
-	var s string
-	err := json.Unmarshal(data, &s)
-	if err != nil {
-		return err
-	}
-
-	var aa2 sdk.AccAddress
-	if len(strings.TrimSpace(s)) == 0 {
-		aa2 = nil
-	}
-
-	bz, err := sdk.GetFromBech32(s, commons.Bech32MainPrefix)
-	if err != nil {
-		return err
-	}
-
-	err = sdk.VerifyAddressFormat(bz)
-	if err != nil {
-		return err
-	}
-	aa2 = bz
-
-	*aa = DesmosAddress{aa2}
-	return nil
-}
-
-// String implements the Stringer interface.
-func (aa DesmosAddress) String() string {
-	if aa.Empty() {
-		return ""
-	}
-
-	bech32Addr, err := bech32.ConvertAndEncode(commons.Bech32MainPrefix, aa.Bytes())
-	if err != nil {
-		panic(err)
-	}
-
-	return bech32Addr
-}
 
 // CreatePostPacketData represents the packet data that should be sent when
 // wanting to create a new post
 type CreatePostPacketData struct {
-	PostCreationData               // Include all the standard data
-	Creator          DesmosAddress `json:"creator"` // Override the creator to make sure it has the proper prefix
+	PostCreationData // Include all the standard data
 }
 
 // NewCreatePostPacketData is the builder function for a new CreatePostPacketData
 func NewCreatePostPacketData(data PostCreationData) CreatePostPacketData {
 	return CreatePostPacketData{
 		PostCreationData: data,
-		Creator:          DesmosAddress{data.Creator},
 	}
 }
 
